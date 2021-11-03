f878afcc1a8e561a413b89be30da56148a7af18c
Denys Burdin <denrowing@gmail.com>
2021-11-03 12:32:19 +0200

    init commit

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true}));
 
-const { userRouter, appartmentRouter} = require('./routes/index')
+const { userRouter, appartmentRouter} = require('./routes/index');
 
 app.use('/users', userRouter);
 app.use('/appartments', appartmentRouter);
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,42 +1,62 @@
-const dbUsers = require('../dataBase/Users')
+const dbUsers = require('../dataBase/Users');
+const fs = require('fs');
 
 module.exports = {
-  getUsers: (req, res) => {
-      res.json(dbUsers);
-  },
+    getUsers: (req, res) => {
+        fs.readFile('/home/denys/WebstormProjects/owu/nodejs-task/dataBase/users.json', 'utf8',
+            (err, data) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log(data.toString());
+            });
+        res.json(dbUsers);
+    },
     createUser: (req, res) => {
         console.log(req.body);
 
-        dbUsers.push({...req.body, id: dbUsers.length + 1})
+        dbUsers.push({...req.body, id: dbUsers.length + 1});
         res.json(dbUsers);
     },
+
     getUserById: (req, res) => {
-        const { user_id } = req.params
-        const user = dbUsers[user_id - 1]
+        const { user_id } = req.params;
+        const user = dbUsers[user_id];
         res.json({ user });
     },
-    deleteUser: (req, res) => {
-        const user_id = req.params.id;
-        dbUsers.splice((user_id - 1), 1)
-        // delete dbUsers[dbUsers.indexOf(dbUsers.filter(function(d){ return d.id == req.params.id; }))]
 
-        // console.log(id);
-        // const userIndex = dbUsers.findIndex(p => p.id === id);
-        // console.log(userIndex);
-        //     dbUsers.splice(userIndex, 1); // const deletedUser =
-        res.json(dbUsers);
-        res.json('User deleted');
+    deleteUser: (req, res) => {
+        // fs.readFile('/home/denys/WebstormProjects/owu/nodejs-task/controllers/dataBase/users.json', 'utf8',
+        //     function(err, data) {
+        //         data = JSON.parse(data);
+        //         delete data[data.indexOf(data.filter(function(d) { return d.id === req.params.id; }))];
+        //         console.log(data);
+        //         fs.writeFile('/home/denys/WebstormProjects/owu/nodejs-task/controllers/dataBase/users.json',
+        //         JSON.stringify(data),
+        //             function(err) {
+        //                 if(err){return console.log(err);}
+        //             });
+        //     });
+
+        const { user_id } = req.params;
+        // dbUsers = dbUsers.filter(user => user.id !== user_id)
+        res.send(`User with the id ${user_id} deleted`);
     },
+
     loginUser: (req, res) => {
         res.json('User is login');
     },
+
     createAppartment: (req, res) => {
         res.json('Appartment is create');
     },
+
     createPost: (req, res) => {
         res.json('User create post');
     },
+
     deletePost: (req, res) => {
         res.json('Users post is delete');
     }
-}
+};
